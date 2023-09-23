cd595c5d2f8c1786d2211e7c9b2e053dcdccf012
LZH <1141947499@qq.com>
2023-09-23 10:59:05 +0800

    hot-fix first commit

--- src/main/java/com/lzh/GitTest.java
+++ src/main/java/com/lzh/GitTest.java
@@ -2,7 +2,10 @@ package com.lzh;
 
 public class GitTest {
     public static void main(String[] args) {
+        //master 指向当前分支最新提交的版本
         System.out.println("Hello Git!!!");
-        System.out.println("Hello Git!!!");
+        System.out.println("Hello Git2!!!");
+        System.out.println("Hello Git3!!!");
+        System.out.println("Hello Git4!!!");
     }
 }
